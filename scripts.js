ca76b4f873c83eb792b6a1bb951e95e6e9347590
Rishi <ri2195@columbia.edu>
2014-02-26 23:39:22 -0500

    Created project plan and document.ready with callback

--- scripts.js
+++ scripts.js
@@ -3,3 +3,30 @@
  */
 
 console.log ("Loaded Page");
+
+/* Project Plan 
+1. Load libraries (jQuery and Google JS)
+2. Setup document.ready function
+3. Load Google Visualization Library and create a callback function
+4. Add data from external JSON file to callback function using .get function
+5. Create an array of arrays of the data, using a for loop
+6. Feed data to visualization library
+7. Create a div in HTML file for visualization
+8. Create a chart using the Google Viz library set the newly created div as its location
+9. Render the chart
+*/
+
+console.log ("Loaded Page");
+
+// 2) Next, we define the callback function for document ready
+
+function stepOne(){
+//Console log to make sure it is working
+console.log("My document must be ready");
+
+}
+
+
+// 1) First, we set up document ready
+
+$(document).ready(stepOne);
